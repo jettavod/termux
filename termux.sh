29abf18203498a81e88ff93ae2c352ffdec800cb
jettavod <81916751+jettavod@users.noreply.github.com>
2021-04-04 19:05:21 +0300

    Update and rename cod.sh to termux.sh

--- termux.sh
+++ termux.sh
@@ -4,25 +4,34 @@ echo "" >/data/data/com.termux/files/usr/etc/motd
 echo "Welcome to Termux!" >>/data/data/com.termux/files/usr/etc/motd
 echo "" >>/data/data/com.termux/files/usr/etc/motd
 apt update -y
+clear
 apt upgrade -y
-apt install -y git zsh curl wget tmux php
+clear
+apt install -y git zsh curl wget tmux php python python2 openssh
+clear
+pip install bpython ipython
+clear
+/data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade pip
+clear
 sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
 termux-reload-settings
 mkdir $HOME/.termux
+clear
 echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" > $HOME/.termux/termux.properties
 termux-reload-settings
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+clear
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+clear
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+clear
 echo "" > .zshrc
-wget https://raw.githubusercontent.com/jettavod/hydravdk/main/Tekst.txt
-cat Tekst.txt > .zshrc
-rm Tekst.txt
-rm cod.sh
+wget https://raw.githubusercontent.com/jettavod/hydravdk/main/zshrcc
+cat zshrcc > .zshrc
+rm zshrcc
+clear
 wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
-apt install openssh -y
-pkg install -y python python2
-pip install bpython ipython
-/data/data/com.termux/files/usr/bin/python3 -m pip install --upgrade pip
 git clone https://github.com/Ranginang67/DarkFly-Tool && cd DarkFly-Tool && chmod +x install.py && python2 install.py && clear && echo "Теперь можно набрать DarkFly"
+rm termux.sh
+exit
